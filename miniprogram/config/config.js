3e8af9d4fb202f1dc1a671f27b1edd956e16f179
szcf <szcf_freedom@163.com>
2025-03-31 17:28:10 +0800

    pp

--- miniprogram/config/config.js
+++ miniprogram/config/config.js
@@ -3,7 +3,9 @@ const config = {
   baseUrl: 'http://192.168.200.16:8991/jeecg-boot/', // 公司
   // baseUrl: 'http://192.168.31.222:8991/jeecg-boot/', // 家
   ossBaseUrl: '',
-  key: '68c29cd2480f9414521bc2069d62a4c2', // 高德key
+  AMAP_KEY: '785345cbbf85e915a22bac4a41e9c808', // 高德Web服务key
+  // AMAP_KEY: '159ecc612329c5d38ba06597f24ea6e2', // 高德Web端key
+  // WECHAT_KEY: '68c29cd2480f9414521bc2069d62a4c2', // 高德微信小程序key
   // ossBaseUrl: 'https://yfbtest.oss-cn-zhangjiakou.aliyuncs.com/',
   versionConfig: {
     sidebar: false,
